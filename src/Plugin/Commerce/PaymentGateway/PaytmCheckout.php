e598d5ecef88b8a09d5eca0b357dd6ca2db694a6
monojithalder <monojithalder@hotmail.com>
2017-03-31 15:23:37 +0530

    Change line indentation

--- src/Plugin/Commerce/PaymentGateway/PaytmCheckout.php
+++ src/Plugin/Commerce/PaymentGateway/PaytmCheckout.php
@@ -36,86 +36,86 @@ use Drupal\commercepaytm\PaytmLibrary;
  */
 class PaytmCheckout extends OffsitePaymentGatewayBase {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
-    dpm($form);
+    /**
+     * {@inheritdoc}
+     */
+    public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+        $form = parent::buildConfigurationForm($form, $form_state);
+        dpm($form);
 
-    $form['merchant_id'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Merchant Id'),
-      '#default_value' => $this->configuration['merchant_id'],
-      '#required' => TRUE,
-    ];
-    $form['merchant_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Merchant Key'),
-      '#default_value' => $this->configuration['merchant_key'],
-      '#required' => TRUE,
-    ];
-    $form['merchant_website'] = [
-          '#type' => 'textfield',
-          '#title' => $this->t('Merchant Website'),
-          '#default_value' => $this->configuration['merchant_website'],
-          '#required' => TRUE,
-    ];
+        $form['merchant_id'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Merchant Id'),
+            '#default_value' => $this->configuration['merchant_id'],
+            '#required' => TRUE,
+        ];
+        $form['merchant_key'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Merchant Key'),
+            '#default_value' => $this->configuration['merchant_key'],
+            '#required' => TRUE,
+        ];
+        $form['merchant_website'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Merchant Website'),
+            '#default_value' => $this->configuration['merchant_website'],
+            '#required' => TRUE,
+        ];
 
 
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-    parent::submitConfigurationForm($form, $form_state);
-    if (!$form_state->getErrors()) {
-      $values = $form_state->getValue($form['#parents']);
-        $this->configuration['merchant_id'] = $values['merchant_id'];
-        $this->configuration['merchant_key'] = $values['merchant_key'];
-        $this->configuration['merchant_website'] = $values['merchant_website'];
+        return $form;
     }
-  }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function onReturn(OrderInterface $order, Request $request) {
-    $paytm_library = new PaytmLibrary();
-    $paramlist = array();
-    $paramlist['RESPCODE']     = $request->get('RESPCODE');
-    $paramlist['RESPMSG']      = $request->get('RESPMSG');
-    $paramlist['STATUS']       = $request->get('STATUS');
-    $paramlist['MID']          = $request->get('MID');
-    $paramlist['TXNAMOUNT']    = $request->get('TXNAMOUNT');
-    $paramlist['ORDERID']      = $request->get('ORDERID');
-    $paramlist['CHECKSUMHASH'] = $request->get('CHECKSUMHASH');
-    $valid_checksum = $paytm_library->verifychecksum_e($paramlist, $this->configuration['merchant_key'], $paramlist['CHECKSUMHASH']);
-    if($valid_checksum) {
-        $a = 0;
-        if($paramlist['STATUS'] == 'TXN_SUCCESS') {
-            $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
-            $payment = $payment_storage->create([
-                'state' => 'authorization',
-                'amount' => $order->getTotalPrice(),
-                'payment_gateway' => $this->entityId,
-                'order_id' => $order->id(),
-                'test' => $this->getMode() == 'test',
-                'remote_id' => $order->id(),
-                'remote_state' => $paramlist['STATUS'],
-                'authorized' => REQUEST_TIME,
-            ]);
-            $payment->save();
-            drupal_set_message($this->t('Your payment was successful with Order id : @orderid and Transaction id : @transaction_id', ['@orderid' => $order->id(), '@transaction_id' => $txnid]));
+    /**
+     * {@inheritdoc}
+     */
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+        parent::submitConfigurationForm($form, $form_state);
+        if (!$form_state->getErrors()) {
+            $values = $form_state->getValue($form['#parents']);
+            $this->configuration['merchant_id'] = $values['merchant_id'];
+            $this->configuration['merchant_key'] = $values['merchant_key'];
+            $this->configuration['merchant_website'] = $values['merchant_website'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onReturn(OrderInterface $order, Request $request) {
+        $paytm_library = new PaytmLibrary();
+        $paramlist = array();
+        $paramlist['RESPCODE']     = $request->get('RESPCODE');
+        $paramlist['RESPMSG']      = $request->get('RESPMSG');
+        $paramlist['STATUS']       = $request->get('STATUS');
+        $paramlist['MID']          = $request->get('MID');
+        $paramlist['TXNAMOUNT']    = $request->get('TXNAMOUNT');
+        $paramlist['ORDERID']      = $request->get('ORDERID');
+        $paramlist['CHECKSUMHASH'] = $request->get('CHECKSUMHASH');
+        $valid_checksum = $paytm_library->verifychecksum_e($paramlist, $this->configuration['merchant_key'], $paramlist['CHECKSUMHASH']);
+        if($valid_checksum) {
+            $a = 0;
+            if($paramlist['STATUS'] == 'TXN_SUCCESS') {
+                $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
+                $payment = $payment_storage->create([
+                    'state' => 'authorization',
+                    'amount' => $order->getTotalPrice(),
+                    'payment_gateway' => $this->entityId,
+                    'order_id' => $order->id(),
+                    'test' => $this->getMode() == 'test',
+                    'remote_id' => $order->id(),
+                    'remote_state' => $paramlist['STATUS'],
+                    'authorized' => REQUEST_TIME,
+                ]);
+                $payment->save();
+                drupal_set_message($this->t('Your payment was successful with Order id : @orderid and Transaction id : @transaction_id', ['@orderid' => $order->id(), '@transaction_id' => $txnid]));
+            }
+            else {
+                drupal_set_message($this->t('Transaction Failed'), 'error');
+            }
         }
         else {
-            drupal_set_message($this->t('Transaction Failed'), 'error');
+            drupal_set_message($this->t('Checksum mismatched.'), 'error');
         }
     }
-    else {
-        drupal_set_message($this->t('Checksum mismatched.'), 'error');
-    }
-  }
 }
